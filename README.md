286851df2383ac85fe40c24df3ca64d542c1f0e1
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-05-31 10:35:11 +0100

    Adds boilerplate.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple OpenStack
+_An easy to use Python 3 library for interacting with items in OpenStack._
+
+
+## License
+[MIT license](LICENSE.txt).
+
+Copyright (c) 2017 Genome Research Limited
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+try:
+    from pypandoc import convert
+    def read_markdown(file: str) -> str:
+        return convert(file, "rst")
+except ImportError:
+    def read_markdown(file: str) -> str:
+        return open(file, "r").read()
+
+setup(
+    name="simpleopenstack",
+    version="1.0.0",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=open("requirements.txt", "r").readlines(),
+    url="https://github.com/wtsi-hgi/simpleopenstack",
+    license="MIT",
+    description="",     # TODO
+    long_description=read_markdown("README.md")
+)
