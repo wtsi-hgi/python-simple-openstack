c9dd29098e6645167fa48a1cc143bbfe3c07570c
EWeatherill <ew7@sanger.ac.uk>
2017-07-12 17:26:41 +0100

    Code to test the real OS managers - needs testing

--- simpleopenstack/tests/test_os_managers.py
+++ simpleopenstack/tests/test_os_managers.py
@@ -1,50 +1,77 @@
-# import unittest
-# from abc import ABCMeta
-#
-# from simpleopenstack.models import OpenstackConnector
-# from simpleopenstack.os_managers import NovaOpenstackKeypairManager
-# from simpleopenstack.os_mock_managers import MockOpenstackKeypairManager, MockOpenstackInstanceManager, \
-#     MockOpenstackImageManager
-# from simpleopenstack.tests._test_managers import OpenstackKeypairManagerTest, OpenstackInstanceManagerTest, \
-#     OpenstackImageManagerTest
-#
-#
-# class _OpenstackItemManagerTest(unittest.TestCase, metaclass=ABCMeta):
-#     """
-#     TODO
-#     """
-#     def setUp(self):
-#         self.openstack_connector = OpenstackConnector()
-#         super().setUp()
-#
-#
-# class NovaOpenstackKeypairManagerTest(OpenstackKeypairManagerTest[NovaOpenstackKeypairManager]):
-#     """
-#     Tests for `NovaOpenstackKeypairManager`.
-#     """
-#     def _create_manager(self) -> NovaOpenstackKeypairManager:
-#         return NovaOpenstackKeypairManager(self.openstack_connector)
-#
-#
-# class NovaOpenstackInstanceManagerTest(OpenstackInstanceManagerTest[MockOpenstackInstanceManager]):
-#     """
-#     TODO
-#     """
-#     def _create_manager(self) -> MockOpenstackInstanceManager:
-#         return MockOpenstackInstanceManager(self.openstack_connector)
-#
-#
-# class GlanceOpenstackImageManagerTest(OpenstackImageManagerTest[MockOpenstackImageManager]):
-#     """
-#     TODO
-#     """
-#     def _create_manager(self) -> MockOpenstackImageManager:
-#         return MockOpenstackImageManager(self.openstack_connector)
-#
-#
-# # TODO: unittest is really stupid and will try to run the below as a test... Probably can add some ignore rules
-# del OpenstackKeypairManagerTest, OpenstackInstanceManagerTest, OpenstackImageManagerTest, _OpenstackItemManagerTest
-# 
-#
-# if __name__ == "__main__":
-#     unittest.main()
+import unittest
+import os
+from abc import ABCMeta
+
+from simpleopenstack.managers import OpenstackInstanceManager, OpenstackImageManager, OpenstackFlavorManager, \
+    OpenstackNetworkManager
+from simpleopenstack.models import OpenstackConnector
+from simpleopenstack.os_managers import NovaOpenstackKeypairManager, RealOpenstackConnector, \
+    NovaOpenstackInstanceManager, GlanceOpenstackImageManager, NeutronOpenstackNetworkManager, \
+    NovaOpenstackFlavorManager
+from simpleopenstack.tests._test_managers import OpenstackKeypairManagerTest, OpenstackInstanceManagerTest, \
+    OpenstackImageManagerTest, OpenstackNetworkManagerTest, OpenstackFlavorManagerTest
+
+class _OpenstackItemManagerTest(unittest.TestCase, metaclass=ABCMeta):
+    """
+    TODO
+    """
+    def setUp(self):
+        self.openstack_connector = RealOpenstackConnector(os.getenv('OS_AUTH_URL'),
+                                                          os.getenv('OS_TENANT'),
+                                                          os.getenv('OS_USERNAME'),
+                                                          os.getenv('OS_PASSWORD'))
+        super().setUp()
+
+
+class NovaOpenstackKeypairManagerTest(
+    _OpenstackItemManagerTest, OpenstackKeypairManagerTest[NovaOpenstackKeypairManager]):
+    """
+    Tests for `NovaOpenstackKeypairManager`.
+    """
+    def _create_manager(self) -> NovaOpenstackKeypairManager:
+        return NovaOpenstackKeypairManager(self.openstack_connector)
+
+
+class NovaOpenstackInstanceManagerTest(
+    _OpenstackItemManagerTest, OpenstackInstanceManagerTest[OpenstackInstanceManager]):
+    """
+    TODO
+    """
+    def _create_manager(self) -> OpenstackInstanceManager:
+        return NovaOpenstackInstanceManager(self.openstack_connector)
+
+
+class GlanceOpenstackImageManagerTest(
+    _OpenstackItemManagerTest, OpenstackImageManagerTest[OpenstackImageManager]):
+    """
+    TODO
+    """
+    def _create_manager(self) -> OpenstackImageManager:
+        return GlanceOpenstackImageManager(self.openstack_connector)
+
+class NovaOpenstackFlavorManagerTest(
+    _OpenstackItemManagerTest, OpenstackFlavorManagerTest[OpenstackFlavorManager]):
+    """
+    Tests for `MockOpenstackFlavorManager`.
+    """
+
+    def _create_manager(self) -> OpenstackFlavorManager:
+        return NovaOpenstackFlavorManager(self.openstack_connector)
+
+class NeutronOpenstackNetworkManagerTest(
+    _OpenstackItemManagerTest, OpenstackNetworkManagerTest[OpenstackNetworkManager]):
+    """
+    Tests for `MockOpenstackNetworkManager`.
+    """
+
+    def _create_manager(self) -> OpenstackNetworkManager:
+        return NeutronOpenstackNetworkManager(self.openstack_connector)
+
+
+# TODO: unittest is really stupid and will try to run the below as a test... Probably can add some ignore rules
+del OpenstackKeypairManagerTest, OpenstackInstanceManagerTest, OpenstackImageManagerTest, _OpenstackItemManagerTest, \
+    OpenstackNetworkManagerTest, OpenstackFlavorManagerTest
+
+
+if __name__ == "__main__":
+    unittest.main()
